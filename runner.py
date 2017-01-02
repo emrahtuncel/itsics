529c2c9f4d9947bb6824f81d3a68c3f47b93b61b
emrah <emrah@ubuntu>
2017-01-02 21:38:41 +0200

    Old comments removed

--- runner.py
+++ runner.py
@@ -1,24 +1,4 @@
 #!/usr/bin/env python
-"""
-@file    runner.py
-@author  Lena Kalleske
-@author  Daniel Krajzewicz
-@author  Michael Behrisch
-@author  Jakob Erdmann
-@date    2009-03-26
-@version $Id: runner.py 19535 2015-12-05 13:47:18Z behrisch $
-
-Tutorial for traffic light control via the TraCI interface.
-
-SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
-Copyright (C) 2009-2015 DLR/TS, Germany
-
-This file is part of SUMO.
-SUMO is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-"""
 
 from __future__ import division
 import os
